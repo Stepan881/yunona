3076f2389b705e768d4423e8817eac9d61a664c5
stepan881 <stepanstepanov180@gmail.com>
2020-12-27 14:24:36 +0300

    add animate menu

--- build/js/_custom.js
+++ build/js/_custom.js
@@ -1,16 +1,40 @@
 jQuery(document).ready(function($){
 
+  // открыть хакрыть меню
+  $(".header__btn").on('click', function(event){
+    if ($( event.target ).closest( ".header__btn" )) {
+      $('.nav-menu').addClass("nav-menu--open");
 
-  $(".header__btn").on('click', function(){
-    $(".nav-menu").addClass("nav-menu--open");
-
+    }
   });
 
-  $(".nav-menu__close").on('click', function(){
-    $(".nav-menu").removeClass("nav-menu--open")
+  $(".nav-menu__close").on('click', function(event){
+    event.preventDefault();
+    if ($( event.target ).closest( ".nav-menu__close" )) {
+      $('.nav-menu').removeClass("nav-menu--open");
+    }
   });
 
+
+  // прокрутка к якорю
+  $(".nav-menu .nav-menu__list a").not("a[href='#']").click(function(evt) {
+
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
 });
+
+
+
+
+
+
+
+
 
 jQuery(window).load(function($) {
   
