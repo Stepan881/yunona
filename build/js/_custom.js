d99e073a523d464c0b37080d9473e8cc2824803c
stepan881 <stepanstepanov180@gmail.com>
2020-12-27 12:44:42 +0300

    add nav-menu

--- build/js/_custom.js
+++ build/js/_custom.js
@@ -1,7 +1,19 @@
 jQuery(document).ready(function($){
 
+
+  $(".header__btn").on('click', function(){
+    $(".nav-menu").addClass("nav-menu--open");
+
+  });
+
+  $(".nav-menu__close").on('click', function(){
+    $(".nav-menu").removeClass("nav-menu--open")
+  });
+
 });
 
 jQuery(window).load(function($) {
   
+
+ 
 });
